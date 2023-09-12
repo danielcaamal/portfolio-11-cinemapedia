62324ce4382245083552333ed3c904cab6369190
daniel caamal <danielcaamal97@gmail.com>
2023-09-11 19:05:39 -0600

    [FTR]: Custom Navigation Bar implemented

--- portfolio-11-cinemapedia-app/portfolio_11_cinemapedia_app/lib/presentation/widgets/widgets.dart
+++ portfolio-11-cinemapedia-app/portfolio_11_cinemapedia_app/lib/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:portfolio_11_cinemapedia_app/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:portfolio_11_cinemapedia_app/presentation/widgets/shared/custom_app_bar.dart';
+export 'package:portfolio_11_cinemapedia_app/presentation/widgets/shared/custom_bottom_navigation_bar.dart';
